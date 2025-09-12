2b266dbdc1b6de0b62a48c6a3caef12c5f4d31ad
yereong <lily030624@gmail.com>
2025-09-13 00:10:39 +0900

    Create Card.tsx
    카드 내용

---
+++ src/component/Project/Card.tsx
@@ -0,0 +1,41 @@
+export const Cards = [
+    {key:'Condin',
+        content:(
+            <>
+                <div className="glass h-[550px] w-[80%] flex flex-col items-start">
+                    <div className="bg-[#353535] w-full h-full">
+                        <button>{'프로젝트 상세 >'}</button>
+                        <img src='./logos/codin.svg' alt="codin"/>
+                    </div>
+
+                </div>
+            </>
+        )
+     },
+      {key:'neeis',
+        content:(
+            <>
+                <div className="glass h-[550px] w-[80%] flex flex-col items-start">
+                    <div className="bg-[#353535] w-full h-full">
+                        <button>{'프로젝트 상세 >'}</button>
+                        <img src='./logos/neeis.svg' alt="codin"/>
+                    </div>
+
+                </div>
+            </>
+        )
+     },
+      {key:'emolog',
+        content:(
+            <>
+                <div className="glass h-[550px] w-[80%] flex flex-col items-start">
+                    <div className="bg-[#353535] w-full h-full">
+                        <button>{'프로젝트 상세 >'}</button>
+                        <img src='./logos/emolog.svg' alt="codin"/>
+                    </div>
+
+                </div>
+            </>
+        )
+     }
+]

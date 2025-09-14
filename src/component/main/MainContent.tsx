bc5c93d36a29160ea8f91dc856f353ab00b02012
yereong <lily030624@gmail.com>
2025-09-15 04:10:31 +0900

    fix: 배경화면 컴포넌트로 분리

--- src/component/main/MainContent.tsx
+++ src/component/main/MainContent.tsx
@@ -1,18 +1,9 @@
-export default function MainContent () {
-
-    return(
-      <div className=" bg-[#000000] flex flex-col items-start justify-center h-svh px-[115px] " >
-            <div className="flex flex-row gap-[30px]">
-                <div className="w-[10px] h-[213px] left-[115px] top-[232px] bg-[#0051FF]"></div>
-                <div className="flex flex-col items-start">
-                    <p className="font-semibold text-[32px] leading-[38px] text-white animate__zoomIn" >안녕하세요!</p>
-                    <p className="font-semibold text-[48px] leading-[57px] text-white">
-                        <span className="text-[#0051FF]">책임감</span>과 <span  className="text-[#0051FF]">소통</span>을 중요시하는 <br/> 
-                        프론트엔드 개발자 <br/>
-                        조예령입니다.
-                    </p>
-                </div>
-            </div>
-      </div>
+import "@/app/globals.css";
+import Background from "./background";
+export default function MainContent() {
+  return (
+    <Background>
+        <div className="text-[50px] text-white font-semibold"></div>
+    </Background>
     );
 }
